e102e343291c14e1fb535e314346c82d284af343
Zan <gesserat@gmail.com>
2015-04-25 21:45:35 +0300

    asdf

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ Definition of models.
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Discount(models.Model):
     owner = models.ForeignKey(User)
     shop = models.TextField()
@@ -12,6 +13,7 @@ class Discount(models.Model):
     discount = models.FloatField()
     expTime = models.DateField()
     description = models.TextField()
+
 
 class Meeting(models.Model):
     metType = models.TextField()
@@ -22,10 +24,12 @@ class Meeting(models.Model):
     money = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
 
+
 class Member(models.Model):
     meeting = models.ForeignKey(Meeting)
     user = models.ForeignKey(User)
     donate = models.DecimalField(max_digits=5, decimal_places=2)
+
 
 class Item(models.Model):
     name = models.TextField()
@@ -33,6 +37,7 @@ class Item(models.Model):
     quality = models.TextField()
     location = models.TextField()
     isCommon = models.BooleanField(default=True)
+
 
 class People(models.Model):
     roomNumber = models.DecimalField(max_digits=5, decimal_places=2)
--- app/views.py
+++ app/views.py
@@ -7,14 +7,16 @@ from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
 
+
 def home(request):
     assert isinstance(request, HttpRequest)
     return render(request, 'app/index.html')
-    
+
 
 def trips(request):
     assert isinstance(request, HttpRequest)
     return render(request, 'app/trips.html')
+
 
 def meetings(request):
     assert isinstance(request, HttpRequest)
