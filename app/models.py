fb17a970538ed9fa084d77c08e09abb5708170e2
Fa-bula <fa_bula@mail.ru>
2015-04-26 09:51:21 +0300

    f O
    O

--- app/models.py
+++ app/models.py
@@ -5,9 +5,14 @@ Definition of models.
 from django.db import models
 from django.contrib.auth.models import User
 
+class Person(models.Model):
+    user = models.OneToOneField(User)
+    roomNumber = models.DecimalField(max_digits=5, decimal_places=0)
+    dormNumber = models.DecimalField(max_digits=5, decimal_places=0)
+
 
 class Discount(models.Model):
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(Person)
     shop = models.TextField()
     discountType = models.TextField()
     discount = models.FloatField()
@@ -19,26 +24,21 @@ class Meeting(models.Model):
     metType = models.TextField()
     time = models.TimeField()
     location = models.TextField()
-    creator = models.ForeignKey(User)
+    creator = models.ForeignKey(Person)
     support = models.TextField()
-    money = models.DecimalField(max_digits=5, decimal_places=2)
+    money = models.DecimalField(max_digits=5, decimal_places=0)
     description = models.TextField()
 
 
 class Member(models.Model):
     meeting = models.ForeignKey(Meeting)
-    user = models.ForeignKey(User)
-    donate = models.DecimalField(max_digits=5, decimal_places=2)
+    user = models.ForeignKey(Person)
+    donate = models.DecimalField(max_digits=5, decimal_places=0)
 
 
 class Item(models.Model):
     name = models.TextField()
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(Person)
     quality = models.TextField()
     location = models.TextField()
     isCommon = models.BooleanField(default=True)
-
-
-class People(models.Model):
-    roomNumber = models.DecimalField(max_digits=5, decimal_places=2)
-    dormNumber = models.DecimalField(max_digits=5, decimal_places=2)
