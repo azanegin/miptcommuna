540a21c7456ffef986ed917bb50c93ca1a60ce07
Fa-bula <fa_bula@mail.ru>
2015-04-26 11:06:13 +0300

    f

--- app/views.py
+++ app/views.py
@@ -1,21 +1,33 @@
 """
 Definition of views.
 """
-from app.models import Meeting
+from app.models import Meeting, Person, Item
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
-
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
     assert isinstance(request, HttpRequest)
-    return render(request, 'app/index.html')
+    context = {'items': Item.objects.all()}
+    return render(request, 'app/index.html', context)
 
 
 def trips(request):
     assert isinstance(request, HttpRequest)
     return render(request, 'app/trips.html')
+
+def person(request, person_id):
+    assert isinstance(request, HttpRequest)
+    context = {'person': get_object_or_404(Person, pk=person_id)}
+    return render(request, 'app/person.html', context)
+
+
+def item(request, item_id):
+    assert isinstance(request, HttpRequest)
+    context = {'item': get_object_or_404(Item, pk=item_id)}
+    return render(request, 'app/item.html', context)
 
 
 def meetings(request):
